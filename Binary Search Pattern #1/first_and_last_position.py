353ef5ce76d373b4863d53423f07cd923b1f2654
pollinariaws <polinazuzuk@gmail.com>
2026-05-02 19:57:29 +0300

    Create first_and_last_position.py
    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
    
    If target is not found in the array, return [-1, -1].
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ Binary Search Pattern #1/first_and_last_position.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.find_left(nums, target)
+        right = self.find_right(nums, target)
+        return [left, right]
+    def find_left(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+        ans = -1
+        while start <= end:
+            mid = (start + end)//2
+            if (nums[mid] < target):
+                start = mid + 1
+            elif (nums[mid] > target):
+                end = mid - 1
+            else:
+                ans = mid
+                end = mid - 1
+        return ans
+    def find_right(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+        ans = -1
+        while start <= end:
+            mid = (start + end)//2
+            if (nums[mid] < target):
+                start = mid + 1
+            elif (nums[mid] > target):
+                end = mid - 1
+            else:
+                ans = mid
+                start = mid + 1
+        return ans
